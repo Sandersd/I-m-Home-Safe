d940887af274226938b3e28923b313eb34f0f9bb
eleanorharding <eleanor@eleanorharding.com>
2015-04-26 00:18:28 +0100

    Initial Front-end
    Front-end content for the app

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -20,6 +20,36 @@ Template.Home.created = function () {
 };
 
 Template.Home.rendered = function () {
+	$( "#addNew" ).click(function() {
+	 	$( "#overlay1" ).toggleClass( "displayNone" );
+	});
+
+	$( "#continue" ).click(function() {
+	 	$( "#overlay1" ).toggleClass( "displayNone" );
+	 	$( "#overlay2" ).toggleClass( "displayNone" );
+	});
+
+	$( "#make" ).click(function() {
+	 	$( "#overlay2" ).toggleClass( "displayNone" );	
+	 	$( "#overlay3" ).toggleClass( "displayNone" );
+	});
+
+	$( "#finish" ).click(function() {
+	 	$( "#overlay3" ).toggleClass( "displayNone" );
+	 	$("#contacts").prepend("<div class='contactCard'><img src='img/d.png'/> <p class='name1'>Dad</p><p class='number1'>+447707019489</p></div>");  
+	});
+
+	$( ".cancel" ).click(function() {
+	 	$( "#overlay1" ).toggleClass( "displayNone" );
+	});
+
+	$( ".cancel2" ).click(function() {
+	 	$( "#overlay2" ).toggleClass( "displayNone" );
+	});
+
+	$( ".cancel3" ).click(function() {
+	 	$( "#overlay3" ).toggleClass( "displayNone" );
+	});
 };
 
 Template.Home.destroyed = function () {
